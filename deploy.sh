57e5ede5460d82a592628b1ce3505c4457a65557
hazardkadam1999 <pratikkadam251999@gmail.com>
2021-09-19 19:05:49 +0530

    Editing Level1

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+vared -p "Enter commit message: " -c message
+
+printf "\e[33m\nBuilding project...\e[39m\n"
+hugo -d ../
+
+printf "\\e[33m\nPushing to YOUR_GITHUB_USERNAME.github.io repository...\e[39m\n\n"
+cd 
+git add .
+git commit -m "$message"
+git push origin main
+printf "\e[32m\nSuccessfully deployed the website!\e[39m"
+
+#printf "\e[33m\n\nNow pushing latest changes to PERSONAL_REPOSITORY_NAME repository...\e[39m\n\n"
+#cd ../
+#git add .
+#git commit -m "$message"
+#git push origin master
+#printf "\033[0;32m\nSuccessfully pushed changes to the repository!\e[39m\n"
